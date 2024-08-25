73e233d10449d81ff087dc5cde869ca76c9c86c4
Mahendra Singh <gurjarmahisingh33@gmail.com>
2024-08-25 14:02:10 +0530

    updated 25/08/24

--- app/main.py
+++ app/main.py
@@ -1,65 +1,39 @@
-import sys, io
+import sys, io 
 sys.stdout = io.StringIO()
 sys.stderr = io.StringIO()
 
-import eel
-import os
-import requests
-from bs4 import BeautifulSoup as bs
+import os,socket, shutil, eel,  requests
+from googlesearch import search
 from urllib.parse import urlparse
-from time import sleep
-from func_timeout import func_timeout
-from random import randrange
+from func_timeout import func_timeout, FunctionTimedOut
 from threading import Thread, Lock
-import shutil
+from time import sleep
 
-eel.init('web')  # The directory containing your HTML files
+# Redirect stdout and stderr to a log file
+log_file = open('log.txt', 'w')
+sys.stdout = log_file
+sys.stderr = log_file
+
+# Handle the base path for resources like HTML files
+if getattr(sys, 'frozen', False):
+    # If the application is run as a bundle (frozen)
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+eel.init(os.path.join(base_path, 'web'))
 
 MAIN_RESPONSE = [1, [], [], []]
 lock = Lock()
 
-@eel.expose
-def scrape(pdf_names, location):
-    location = location.replace('\\', '/')
-    pdf_names = pdf_names.replace(':', '_')
-    pdf_names = pdf_names.split(',')
-    MAIN_RESPONSE[1].clear()
-    MAIN_RESPONSE[2].clear()
-    MAIN_RESPONSE[3].clear()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
 
-    try:
-        delete_dirs(location)
-    except OSError as e:
-        response = [0, 'path not found']
-        return response
-
-    try:
-        response = []
-        for pdf_name in pdf_names:
-            pdf_name = pdf_name.strip()
-            if pdf_name[-3:] != "pdf":
-                pdf_name = pdf_name + " pdf"
-            response = scrapping(pdf_name, location)
-        return response
-    except Exception as e:
-        print(e)
-        response = [1, 'cant download pdf']
-        return response
-
-
-@eel.expose
-def sendData():
-    return MAIN_RESPONSE
-
-def savePdf(link, location, downloaded, notDownloaded):
-    parsed_url = urlparse(link)
-    pdf_name = parsed_url.path.split('/')[-1]
-    try:
-        pdf_name = pdf_name.replace(':', "_")
-    except:...
+def savePdf(link, location, downloaded, notDownloaded, pdf_name):
     file_path = os.path.join(location, pdf_name)
     try:
-        response = requests.get(link, stream=True, timeout=30)  # request for pdf page
+        response = requests.get(link, stream=True, timeout=30, headers=headers)  # request for pdf page
         if response.status_code == 200:
             content_length = response.headers.get('Content-Length')
             with open(file_path, 'wb') as f:
@@ -75,6 +49,7 @@ def savePdf(link, location, downloaded, notDownloaded):
         else:
             if os.path.exists(file_path):
                 os.remove(file_path)
+            else:...
             with lock:
                 notDownloaded.append(link)
             return
@@ -87,13 +62,85 @@ def savePdf(link, location, downloaded, notDownloaded):
         # Delete partially downloaded file
         if os.path.exists(file_path):
             os.remove(file_path)
+        else:...
 
-def goto_savePdf(link, location, downloaded, notDownloaded):
+def goto_savePdf(link, location, downloaded, notDownloaded, pdf_name):
     try:
-        x = func_timeout(60, savePdf, [link, location, downloaded, notDownloaded])  # 60 seconds timeout for savePdf() function
-        print(x)
-        return True
+        func_timeout(60, savePdf, args=[link, location, downloaded, notDownloaded, pdf_name])
+    except FunctionTimedOut:
+        with lock:
+            notDownloaded.append(link)
+            status = f'NOT downloaded (timeout): {link}'
+            eel.update_status(status)
+    except Exception as e:
+        with lock:
+            notDownloaded.append(link)
+            status = f'NOT downloaded (timeout or other error): {link}'
+            eel.update_status(status)
+
+def scrapping(topic_name, location):
+    status = f'Finding for \"{topic_name}\"'
+    eel.update_status(status)
+
+    status = f'Initializing....'
+    eel.update_status(status)
+
+    try:
+        topic_location = os.path.join(location, topic_name)
+        os.makedirs(topic_location, exist_ok=True)
     except Exception as e:...
+
+    status = f"Searching for PDF's...."
+    eel.update_status(status)
+
+    # Simulate Google search
+    links_list = []
+    try:
+        for j in search(topic_name, tld="co.in", num=200, stop=200, pause=3, user_agent=headers['User-Agent']):
+            is_pdf = j[-4:]
+            if is_pdf == ".pdf":
+                links_list.append(j)
+    except requests.exceptions.RequestException as e:
+        if e.response.status_code == 429:
+            return [0, 'Return with Error code- 429 "Too many requests please try again after 1 hour" ']
+
+    # Remove duplicates
+    links_list = set(links_list)
+
+    downloaded = []
+    notDownloaded = []
+    threads = []
+
+    status = f"Fetching PDF links...."
+    eel.update_status(status)
+
+    for link in links_list:
+        parsed_url = urlparse(link)
+        pdf_name = parsed_url.path.split('/')[-1]
+        try:
+            pdf_name = pdf_name.replace(':', "_")
+        except:
+            pass
+        try:
+            t = Thread(target=goto_savePdf, args=(link, topic_location, downloaded, notDownloaded,pdf_name))
+            t.start()
+            threads.append(t)
+        except Exception as e:...
+    for t in threads:
+        t.join()
+
+    response = [2, list(links_list), list(downloaded), list(notDownloaded)]
+    MAIN_RESPONSE[1].extend(links_list)
+    MAIN_RESPONSE[2].extend(downloaded)
+    MAIN_RESPONSE[3].extend(notDownloaded)
+    for pdf in notDownloaded:
+            path = f'{location}/{topic_name}/{pdf_name}'
+            try:
+                if os.path.exists(path):
+                        os.remove(path)
+            except Exception as e :...
+    
+    return response
 
 def delete_dirs(location):
     try:
@@ -105,118 +152,62 @@ def delete_dirs(location):
             elif os.path.isdir(path):
                 shutil.rmtree(path)
             else:...
+    except OSError as e:...
 
+@eel.expose
+def sendData():
+    return MAIN_RESPONSE
+
+@eel.expose
+def scrape(pdf_names, location):
+    location = location.replace('\\', '/')
+    pdf_names = pdf_names.replace(':', '_')
+    pdf_names = pdf_names.split(',')
+    MAIN_RESPONSE[1].clear()
+    MAIN_RESPONSE[2].clear()
+    MAIN_RESPONSE[3].clear()
+    try:
+        socket.create_connection(("1.1.1.1", 80))
+    except:
+            return [0,'Oops! No internet connection. Please check your connection and try again.']
+    try:
+        delete_dirs(location)
     except OSError as e:
-        os.makedirs(location, exist_ok=True)
-def scrapping(topic_name, location):
-    status = f'Finding for \"{topic_name}\"'
-    eel.update_status(status)
-
-    status = f'Initializing....'
-    eel.update_status(status)
+        response = [0, 'path not found']
+        return response
 
     try:
-        topic_location = os.path.join(location, topic_name)
-        os.makedirs(topic_location, exist_ok=True)
+        response = []
+        for pdf_name in pdf_names:
+            pdf_name = pdf_name.strip()
+            if pdf_name[-3:] != "pdf":
+                pdf_name = pdf_name + " pdf"
+            response = scrapping(pdf_name, location)
+        return response
     except Exception as e:
-        print(f"Error creating directory {topic_location}: {e}")
+        response = [1, 'Somthing went wrong please try again']
+        return response
 
-    # Simulate Google search
-    search_url = f"https://www.google.com/search?q={topic_name}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    response = requests.get(search_url, headers=headers)
-    soup = bs(response.text, 'html.parser')
-    
-    status = f"Searching for PDF's...."
-    eel.update_status(status)
-    # find all pages links
-    next_pages = soup.select('a[aria-label^="Page"]')
-
-    url =f"https://www.google.com{next_pages[0]['href']}"
-    next_pages_links = []
-    for num in range(1, 20):
-        if num != 1:
-            old = f'start={num * 10 - 10}'
-        else:
-            old = f'start={num * 10}'
-        new = f'start={num * 10}'
-        url = url.replace(old, new)
-        next_pages_links.append(url)
-
-    status = f"Fetching PDF links...."
-    eel.update_status(status)
-
-    links_list = []
-    threads = []
-
-    def extract_pdf_links(page_url):
-        page_response = requests.get(page_url, headers=headers)
-        page_soup = bs(page_response.text, 'html.parser')
-        pdf_links = page_soup.select('a[href$=".pdf"]')
-        with lock:
-            for link in pdf_links:
-                href = link['href']
-                links_list.append(href)
-
-    for page_url in next_pages_links:
-        thread = Thread(target=extract_pdf_links, args=(page_url,))
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
-    # Remove duplicates
-    links_list = set(links_list)
-
-    downloaded = []
-    notDownloaded = []
-    threads = []
-
-    for link in links_list:
-        try:
-            t = Thread(target=goto_savePdf, args=(link, topic_location, downloaded, notDownloaded))
-            t.start()
-            threads.append(t)
-        except Exception as e:
-            print(f"Error starting thread for {link}: {e}")
-    for t in threads:
-        t.join()
-
-    response = [2, list(links_list), list(downloaded), list(notDownloaded)]
-    MAIN_RESPONSE[1].extend(links_list)
-    MAIN_RESPONSE[2].extend(downloaded)
-    MAIN_RESPONSE[3].extend(notDownloaded)
-    return response
-
+# Check available port
+def find_free_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]
 
 def start_eel():
     try:
+        port = find_free_port()
         eel.start('main.html', mode='chrome',
                   host='localhost',
-                  port=27000,
+                  port=port,        
                   block=True,
-                  size=(500, 480),
+                  size=(550, 650),
                   position=(300, 50),
                   disable_cache=True,
                   cmdline_args=['--browser-startup-dialog',
                                 '--incognito', '--no-experiments'])
     except Exception as e:
-        print(f"Error starting Eel: {e}")
+        pass
 
 if __name__ == '__main__':
     start_eel()
-
-
-
-# for page_url in next_pages_links:
-#         try:
-#             t = Thread(target=extract_pdf_links, args=(page_url))
-#             t.start()
-#             threads.append(t)
-#         except Exception as e:
-#             print(e)
-#     for t in threads:
-#         t.join()
--- app/web/main.js
+++ app/web/main.js
@@ -1,102 +1,132 @@
-function getData() {
+// Check internet connectivity after 3 seconds on page load
+window.onload = () => {
+      setTimeout(checkInternet, 3000);
+};
+
+function checkInternet() {
+      if (!navigator.onLine) {
+            alert('You are offline');
+      }
+}
+
+document.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+})
+let state = 0
+
+// Disable  key combinations for security
+document.onkeydown = (e) => {
+      const forbiddenKeys = [
+            { key: 'F12' },
+            { key: 'I', ctrlKey: true, shiftKey: true },
+            { key: 'C', ctrlKey: true, shiftKey: true },
+            { key: 'J', ctrlKey: true, shiftKey: true },
+            { key: 'N', ctrlKey: true, shiftKey: true },
+            { key: 'P', ctrlKey: true, shiftKey: true },
+            { key: 'R', ctrlKey: true, shiftKey: true },
+            { key: 'u', ctrlKey: true },
+            { key: 'p', ctrlKey: true },
+            { key: 'r', ctrlKey: true },
+            { key: 'a', ctrlKey: true },
+            { key: 'n', ctrlKey: true },
+            { key: 't', ctrlKey: true },
+            { key: 's', ctrlKey: true },
+            { key: 'o', ctrlKey: true },
+            { key: 'j', ctrlKey: true },
+      ];
+
+      forbiddenKeys.forEach(forbiddenKey => {
+            if (
+                  e.key === forbiddenKey.key &&
+                  (forbiddenKey.ctrlKey === undefined || e.ctrlKey === forbiddenKey.ctrlKey) &&
+                  (forbiddenKey.shiftKey === undefined || e.shiftKey === forbiddenKey.shiftKey)
+            ) {
+                  e.preventDefault();
+                  return false; // Stop event propagation
+            }
+      });
+};
+
+// Fetch data from Python's eel function and display results
+async function getData() {
       eel.sendData()(function (response) {
 
             try {
-                  console.log(response)
-
-
                   response[1].forEach((item, index) => {
                         console.log(item)
                         document.getElementById('all-link').insertAdjacentHTML('beforeend', `<td>${index + 1} : <a style=" text-decoration:none; color:darkblue;" target="_blank" href="${item}">${item} </a>   </td>`)
-
                   });
                   response[2].forEach((item, index) => {
                         console.log(item)
                         document.getElementById('first-link').insertAdjacentHTML('beforeend', `<td> ${index + 1} : <a style=" text-decoration:none; color:darkblue;" target="_blank" href="${item}">${item} </a>`)
-
                   });
                   response[3].forEach((item, index) => {
                         console.log(item)
                         document.getElementById('second-link').insertAdjacentHTML('beforeend', `<td> ${index + 1} : <a style=" text-decoration:none; color:darkblue;" target="_blank" href="${item}">${item} </a>`)
-
                   });
                   document.getElementById('all').style.display = 'block'
                   document.getElementById('first').style.display = 'block'
                   document.getElementById('second').style.display = 'block'
                   document.getElementById('result').innerText = 'Result'
-
+                  // document.getElementById('result').style.backgroundColor = 'cyan'
             }
             catch (err) {
                   console.log(err)
             }
 
       })
-
 }
 
-
+// Trigger search and disable button during the process
 async function search() {
-      var pdf_name = document.getElementById("pdf-name").value;
-      var location = document.getElementById("location").value;
+      var pdf_name = document.getElementById("pdf-name").value
+      var location = document.getElementById("location").value
       const submitButton = document.getElementById('btn');
       if (submitButton) {
             submitButton.disabled = true;
-            console.log('licked')
       }
       document.getElementById('log').style.display = 'block'
       document.getElementById('result').innerText = ''
-
-
-
-      eel.scrape(pdf_name, location)(call_Back);
+      state = 1
+      eel.scrape(pdf_name, location)(callBack);
 }
-
+// Update status log
 function update_status(status) {
-      console.log(status)
-
-      document.getElementById('log-list').insertAdjacentHTML('beforeend', `:: ${status} <br>`)
-
-
+      console.log(status);
+      document.getElementById('log-list').insertAdjacentHTML('beforeend', `>>> ${status} <br>`);
 }
 
-
-
-function call_Back(response) {
-      // document.getElementById('log').style.display = 'none'
-
-      console.log(response)
+// Callback for handling scrape response
+function callBack(response) {
+      state = 0
       const submitButton = document.getElementById('btn');
       if (submitButton) {
             submitButton.disabled = false;
-            console.log('licked')
-
       }
-      if (response[0] == 2) {
-            window.location.href = 'main.html'
+
+      const resultElement = document.getElementById('result');
+      const logElement = document.getElementById('log');
+
+      if (response[0] === 2) {
+            window.location.href = 'main.html';
       }
       else if (response[0] == 0) {
-            console.log('its zero')
+            resultElement.innerText = response[1];
+            resultElement.style.backgroundColor = 'red';
+            document.getElementById('log-list').insertAdjacentHTML('beforeend', ``)
             document.getElementById('log').style.display = 'none'
-            document.getElementById('result').style.backgroundColor = 'red'
-            document.getElementById('result').innerText = 'Path does not exist! make sure folder that you choose exist or path is Right'
-
+            // document.getElementById('result').innerText = 'Path does not exist! make sure folder that you choose exist or path is Right'
       }
       else {
-
-            console.log('its one')
-            document.getElementById('result').innerText = 'something went wrong'
-            document.getElementById('result').style.backgroundColor = 'red'
+            resultElement.innerText = response[1];
+            resultElement.style.backgroundColor = 'red';
+            document.getElementById('log-list').insertAdjacentHTML('beforeend', ``)
             document.getElementById('log').style.display = 'none'
-
-
-
-
       }
-
-
-
 }
 
-
-
-
+window.addEventListener('beforeunload', function (e) {
+      if (state == 1) {
+            e.preventDefault();
+      }
+});
